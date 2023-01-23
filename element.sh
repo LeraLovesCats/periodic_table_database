f4359cbded9a83e62656aa3aac86c9cd8b8fbe94
LeraLovesCats <crazypotatoness@yandex.ru>
2023-01-23 17:16:59 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table tuples-only -c"
+
+echo -e "\nPlease provide an element as an argument"
+read ELEMENT_NAME
+
+The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
+
+
+I could not find that element in the database.
